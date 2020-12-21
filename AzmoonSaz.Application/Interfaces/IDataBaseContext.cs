759088926151afb655e3d55aeddb4540e10df82f
Matin Babaki <matinking82@gmail.com>
2020-12-21 14:58:50 +0330

    Add Save Change To Interface

--- AzmoonSaz.Application/Interfaces/IDataBaseContext.cs
+++ AzmoonSaz.Application/Interfaces/IDataBaseContext.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AzmoonSaz.Application.Interfaces
@@ -17,5 +18,12 @@ namespace AzmoonSaz.Application.Interfaces
         DbSet<Classroom> Classrooms { get; set; }
         DbSet<Test> Tests { get; set; }
         DbSet<Question> Questions { get; set; }
+
+
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,CancellationToken cancellationToken = new CancellationToken());
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
     }
 }

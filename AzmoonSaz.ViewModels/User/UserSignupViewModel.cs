180ecf8cf393c8529e62390699374332b62bda11
Matin Babaki <matinking82@gmail.com>
2020-12-21 13:16:58 +0330

    Add ViewModels

---
+++ AzmoonSaz.ViewModels/User/UserSignupViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.ViewModels.User
+{
+    public record UserSignupViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string UserName { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string ConfirmPassword { get; set; }
+    }
+}

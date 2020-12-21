8296d96c3cbb74e968d8457fab231cb553ebd17d
Matin Babaki <matinking82@gmail.com>
2020-12-21 14:23:39 +0330

    Add service status

---
+++ AzmoonSaz.Common/Enums/ServiceStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.Common.Enums
+{
+    public enum ServiceStatus
+    {
+        Success,
+        SystemError,
+        NotFound,
+        InputParametersError,
+        AccessDenied,
+        SaveFileError,
+        Error
+    }
+}

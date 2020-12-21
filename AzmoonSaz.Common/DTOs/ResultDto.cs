47b628c4655577c9c9621c1065a727fc061e0b03
Matin Babaki <matinking82@gmail.com>
2020-12-21 14:24:42 +0330

    Add ResultDto

---
+++ AzmoonSaz.Common/DTOs/ResultDto.cs
@@ -0,0 +1,19 @@
+﻿using AzmoonSaz.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.Common.DTOs
+{
+    public record ResultDto
+    {
+        public ServiceStatus Status { get; set; }
+        public string Message { get; set; }
+    }
+    public record ResultDto<T> : ResultDto
+    {
+        public T Data { get; set; }
+    }
+}

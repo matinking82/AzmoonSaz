d48a915e40d122ccb49bf62398643658bf546655
Matin Babaki <matinking82@gmail.com>
2020-12-21 16:05:06 +0330

    add utilities

---
+++ EndPoint.Site/Utilities/ClaimUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Utilities
+{
+    public static class ClaimUtilities
+    {
+        public static int GetUserId(this ClaimsPrincipal User)
+        {
+            return int.Parse(((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
---
+++ EndPoint.Site/Utilities/LoginManager.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Utilities
+{
+    public static class LoginManager
+    {
+        public static void LoginToSite(this HttpContext httpContext, long UserId, string Name)
+        {
+            var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier,UserId.ToString()),
+                    new Claim(ClaimTypes.Name, Name),
+                };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+            var properties = new AuthenticationProperties()
+            {
+                IsPersistent = true,
+            };
+            httpContext.SignInAsync(principal, properties);
+        }
+
+        public static async Task LoginToSiteAsync(this HttpContext httpContext, long UserId, string Name)
+        {
+            await Task.Run(() =>
+            {
+                var claims = new List<Claim>()
+                {
+                      new Claim(ClaimTypes.NameIdentifier,UserId.ToString()),
+                      new Claim(ClaimTypes.Name, Name),
+                };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                var properties = new AuthenticationProperties()
+                {
+                    IsPersistent = true,
+                };
+                httpContext.SignInAsync(principal, properties);
+            });
+        }
+
+        public static void SignoutSite(this HttpContext httpContext)
+        {
+            httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        public static async Task SignoutSiteAsync(this HttpContext httpContext)
+        {
+            await Task.Run(() =>
+            {
+                httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            });
+        }
+    }
+}

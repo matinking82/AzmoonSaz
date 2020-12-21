c38007628760d1f4f09292f57445e1c1bc4c25d1
Matin Babaki <matinking82@gmail.com>
2020-12-21 14:37:36 +0330

    Add SignupService

---
+++ AzmoonSaz.Application/Services/Interfaces/IUserServices.cs
@@ -0,0 +1,18 @@
+﻿using AzmoonSaz.Common.DTOs;
+using AzmoonSaz.Common.DTOs.User;
+using AzmoonSaz.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.Application.Services.Interfaces
+{
+    public interface IUserServices
+    {
+        Task<bool> AddUserAsync(User user);
+
+        Task<ResultDto> SignupUserAsync(RequestSignupUserDto request);
+    }
+}
---
+++ AzmoonSaz.Application/Services/UserServices.cs
@@ -0,0 +1,70 @@
+﻿using AzmoonSaz.Application.Interfaces;
+using AzmoonSaz.Application.Services.Interfaces;
+using AzmoonSaz.Common.DTOs;
+using AzmoonSaz.Common.DTOs.User;
+using AzmoonSaz.Common.Enums;
+using AzmoonSaz.Common.Utilities;
+using AzmoonSaz.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.Application.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IDataBaseContext _context;
+        public UserServices(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> AddUserAsync(User user)
+        {
+            return Task.Run(async () =>
+            {
+                try
+                {
+                    await _context.Users.AddAsync(user);
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public async Task<ResultDto> SignupUserAsync(RequestSignupUserDto request)
+        {
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    User newUser = new User()
+                    {
+                        UserName = request.UserName,
+                        Password = await request.Password.ToHashedAsync(),
+                    };
+
+                    await AddUserAsync(newUser);
+
+                    return new()
+                    {
+                        Status = ServiceStatus.Success
+                    };
+                }
+                catch (Exception)
+                {
+                    return new ResultDto()
+                    {
+                        Status = ServiceStatus.SystemError,
+                    };
+                }
+            });
+        }
+    }
+}

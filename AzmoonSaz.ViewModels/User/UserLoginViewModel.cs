cee2705095d03ded1bc6f36aa550b79197daa68d
Matin Babaki <matinking82@gmail.com>
2020-12-21 16:06:21 +0330

    user login view model

---
+++ AzmoonSaz.ViewModels/User/UserLoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonSaz.ViewModels.User
+{
+    public record UserLoginViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string UserName { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Password { get; set; }
+    }
+}
